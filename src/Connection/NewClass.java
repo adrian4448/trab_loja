1a507fed963e384f20fb2455d2395f2f0fe189e8
AdrianMorais <adrian.morais4448@gmail.com>
2020-11-02 21:06:07 -0300

    Start Project

---
+++ src/Connection/NewClass.java
@@ -0,0 +1,12 @@
+package Connection;
+
+public class NewClass {
+    
+    public static void main(String[] args) {
+      DB conection = new DB();
+    
+      conection.getConnection();
+    }
+    
+    
+}
